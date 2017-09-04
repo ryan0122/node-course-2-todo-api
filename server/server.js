5ed45022227e9a509acfb9db8aaac7309faa8568
ryan0122 <ryan.mack1200@gmail.com>
2017-09-04 12:41:24 -0600

    added GET route

--- server/server.js
+++ server/server.js
@@ -18,9 +18,17 @@ app.post('/todos', (req, res) => {
   todo.save()
     .then((doc)=> {
       res.send(doc);
-  }, (e) =>{
+    }, (e) =>{
       res.status(400).send(e);
     })
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
