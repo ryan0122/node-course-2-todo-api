42165380d694c6a2c6f9afcbba445f7f47a41f02
ryan0122 <ryan.mack1200@gmail.com>
2017-09-04 23:31:58 -0600

    fixed case

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
+var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
