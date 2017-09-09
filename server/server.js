db920fdc177349da70cc371f8d0ed81036555292
ryan0122 <ryan.mack1200@gmail.com>
2017-09-08 23:39:14 -0600

    added PATCH route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
-
-var express = require('express');
-var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +26,7 @@ app.post('/todos', (req, res) => {
     });
 });
 
+//READ todos
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -35,7 +36,6 @@ app.get('/todos', (req, res) => {
 });
 
 //GET single
-
 app.get('/todos/:id', (req, res) => {
 
   var id = req.params.id;
@@ -57,7 +57,7 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-//Delete single
+//DELETE single
 app.delete('/todos/:id', (req, res) => {
   //get id
   var id = req.params.id;
@@ -76,6 +76,38 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+//UPDATE single
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  //only use the keys specified
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  //validate the id, not valid send 404
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    })
+    .catch((e) => {
+
+    })
+
 });
 
 app.listen(port, () => {
