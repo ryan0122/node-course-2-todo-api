f1c43345263b93aa420b2aebf4e83a55e6e40053
ryan0122 <ryan.mack1200@gmail.com>
2017-09-18 23:29:54 -0600

    added users/login route

--- server/server.js
+++ server/server.js
@@ -130,8 +130,24 @@ app.post('/users', (req, res) => {
     });
 });
 
+//GET /users/me
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+//POST /users/login
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token)=>{
+      res.header('x-auth', token).send(user);
+    })
+
+  }).catch((e) => {
+    res.send(400);
+  });
+
 });
 
 app.listen(port, () => {
